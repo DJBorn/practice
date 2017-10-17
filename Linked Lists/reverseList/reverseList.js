98acf442b194ea0f99300bd33c24ac0d0781cb52
ilgoo jung <idjung4@gmail.com>
2017-10-17 15:27:41 -0400

    reverseList JavaScript

---
+++ Linked Lists/reverseList/reverseList.js
@@ -0,0 +1,32 @@
+var reverseList = function(head) {
+    if(!head)
+        return null;
+    var last = null;
+    var cur = head;
+    var next = null;
+    while(cur) {
+        next = cur.next;
+        cur.next = last;
+        last = cur;
+        cur = next;
+    }
+    return last;
+}
+
+var list = {
+    data: 1,
+    next: {
+        data: 2,
+        next: {
+            data:3,
+            next: null
+        }
+    }
+};
+
+console.log("Reverse 1 -> 2 -> 3");
+console.log(reverseList(list));
+console.log("Reverse null");
+console.log(reverseList(null));
+console.log("Reverse 1");
+console.log(reverseList({data:1, next: null}));
