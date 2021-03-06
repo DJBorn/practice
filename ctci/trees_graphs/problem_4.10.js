16aead2894932acf87282bc9de4b7dd8deb9333d
Daniel Jung <idjung4@gmail.com>
2021-03-06 00:27:05 -0500

    Added solution to problem 4.10 in ctci

---
+++ ctci/trees_graphs/problem_4.10.js
@@ -0,0 +1,66 @@
+// Check Subtree
+// T1 and T2 are two very large binary trees, with T1 much bigger than T2. Create an algorithm to determine if T2 is a subtree of T1.
+// A tree T2 is a subtree of T1 if there exists a node n in T1 such that the subtree of n is identical to T2. That is, if you cut off the tree at node n, the two trees would be identical.
+
+class TreeNode {
+	constructor(data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+function checkSubtree(headA, headB) {
+	let treeAInOrder = getInOrderArray(headA).join('');
+	let treeBInOrder = getInOrderArray(headB).join('');
+
+	return treeAInOrder.indexOf(treeBInOrder) >= 0;
+}
+
+function getInOrderArray(head) {
+	ans = [];
+	function inOrderRecurse(head) {
+		if(head === null) {
+			ans.push("X");
+			return;
+}
+		ans.push("#" + head.data);
+		inOrderRecurse(head.left);
+		inOrderRecurse(head.right);
+	}
+	inOrderRecurse(head);
+	return ans;
+}
+
+
+
+
+let node1 = new TreeNode(9);
+let node2 = new TreeNode(6);
+let node3 = new TreeNode(12);
+let node4 = new TreeNode(3);
+let node5 = new TreeNode(7);
+let node6 = new TreeNode(11);
+let node7 = new TreeNode(14);
+let node8 = new TreeNode(1);
+
+node1.left = node2;
+node1.right = node3;
+node2.left = node4;
+node2.right = node5;
+node3.left = node6;
+node3.right = node7;
+node2.left = node4;
+node2.right = node5;
+node3.left = node6;
+node3.right = node7;
+node4.left = node8;
+
+console.assert(checkSubtree(node1, node2) === true);
+console.assert(checkSubtree(node1, node3) === true);
+console.assert(checkSubtree(node1, node4) === true);
+console.assert(checkSubtree(node1, node5) === true);
+console.assert(checkSubtree(node1, node6) === true);
+console.assert(checkSubtree(node1, node7) === true);
+console.assert(checkSubtree(node1, node8) === true);
+console.assert(checkSubtree(node2, node1) === false);
--- ctci/trees_graphs/problem_4.9.js
+++ ctci/trees_graphs/problem_4.9.js
@@ -1,7 +1,6 @@
-// First Common Ancestor
-// Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not necessarily a binary search tree.
+// BST Sequences
+// A binary search tree was created by traversing through an array from left to right and inserting each element. Given a binary search tree with distinct elements, print all possible arrays that could have led to this tree.
 
-// O()
 class TreeNode {
 	constructor(data) {
 		this.data = data;
